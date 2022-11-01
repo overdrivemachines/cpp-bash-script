f3a6f72186ee1b4e0cd4cc69270b3de48f2ed9e4
get.dipen@gmail.com <get.dipen@gmail.com>
2022-10-31 21:59:07 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# cpp-bash-script
+# CPP Bash Script
+
 A bash script that will create a new C++ project
---
+++ cpp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+project_name=$1
+echo "Creating C++ Project $project_name"
+
+# Make directory
+mkdir $project_name
+
