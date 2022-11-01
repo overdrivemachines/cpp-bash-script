367b66a1c50064d2a4390d28523a4f2ae082fd00
get.dipen@gmail.com <get.dipen@gmail.com>
2022-10-31 22:07:42 -0700

    added .gitignore and readme

--- cpp.sh
+++ cpp.sh
@@ -6,3 +6,17 @@ echo "Creating C++ Project $project_name"
 # Make directory
 mkdir $project_name
 
+
+# Save .gitignore file
+cd $project_name
+wget https://raw.githubusercontent.com/overdrivemachines/cpp-bash-script/main/.gitignore
+
+# Append executable name at the end of .gitignore file
+echo "$project_name" >> .gitignore
+
+# Create README
+echo "# $project_name" >> README.md
+
+# Create CPP file
+
+# Create Makefile
