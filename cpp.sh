57569b9417ad893237a5feb8d0565931632630db
get.dipen@gmail.com <get.dipen@gmail.com>
2022-10-31 22:29:59 -0700

    added cpp, makefile and initialized git

--- cpp.sh
+++ cpp.sh
@@ -12,11 +12,25 @@ cd $project_name
 wget https://raw.githubusercontent.com/overdrivemachines/cpp-bash-script/main/.gitignore
 
 # Append executable name at the end of .gitignore file
-echo "$project_name" >> .gitignore
+echo "
+$project_name" >> .gitignore
 
 # Create README
 echo "# $project_name" >> README.md
 
-# Create CPP file
+# Download CPP file
+wget https://raw.githubusercontent.com/overdrivemachines/cpp-bash-script/main/main.cpp
+mv main.cpp $project_name.cpp
 
-# Create Makefile
+
+# Download Makefile
+wget https://raw.githubusercontent.com/overdrivemachines/cpp-bash-script/main/Makefile
+sed -i "s/main/$project_name/g" Makefile
+
+# Initialize git
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin git@github.com:overdrivemachines/$project_name.git
+git push -u origin main
